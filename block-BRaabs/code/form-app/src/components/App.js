67b306c61a3529bd3f87e6f7880644c7846fb32f
tinkalguli <tinkalmunu@gmail.com>
2021-02-05 14:21:40 +0530

    Complete Controlled Component

---
+++ block-BRaabs/code/form-app/src/components/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import FormContainer from "./FormContainer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <FormContainer />
+    </>
+  )
+}
+
+export default App;
