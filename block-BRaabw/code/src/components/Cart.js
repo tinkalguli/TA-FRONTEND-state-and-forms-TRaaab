e6ddac0cdd747918fecd6f775a90af121cb675b1
tinkalguli <tinkalmunu@gmail.com>
2021-02-10 18:31:56 +0530

    Add localhost support

--- block-BRaabw/code/src/components/Cart.js
+++ block-BRaabw/code/src/components/Cart.js
@@ -1,16 +1,20 @@
 import React from "react";
 
 export default function Cart(props) {
-  if(props.products.length) {
-    var totalCount = props.products.map(item => item.count)
-    .reduce((a, c) => a + c, 0);
-
-    var totalPrice = props.products.map(item => item.totalPrice)
-      .reduce((a, c) => a + c, 0).toFixed(2);
-    
-    var installments = props.products.map(item => item.installments)
-      .sort((a, b) => b - a)[0];
-  }
+    const products = props.products;
+    let totalCount = 0;
+    let totalPrice = "0.00";
+    let installments;
+    if(products.length) {
+      totalCount = products.map(item => item.count)
+      .reduce((a, c) => a + c, 0);
+  
+      totalPrice = products.map(item => item.totalPrice)
+        .reduce((a, c) => a + c, 0).toFixed(2);
+      
+      installments = products.map(item => item.installments)
+        .sort((a, b) => b - a)[0];
+    }
 
   return (
     <aside className={props.isCartVisible ? "cart" : "cart cart-disable"}>
@@ -26,20 +30,20 @@ export default function Cart(props) {
             className="toggle-btn">
             <div className="bag-img-div">
               <img src="/static/bag-icon.png" alt="bag icon"/>
-              <p className="total-count">{props.products.length ? totalCount : "0"}</p>
+              <p className="total-count">{totalCount}</p>
             </div>
           </button>
       }
       <div className="flex-center cart-title">
         <div className="bag-img-div">
           <img className="cart-icon-inside" src="/static/bag-icon.png" alt="bag icon" />
-          <p className="total-count">{props.products.length ? totalCount : "0"}</p>
+          <p className="total-count">{totalCount}</p>
         </div>
         <h3>Cart</h3>
       </div>
       <ul className="cart-items">
         {
-          !props.products.length
+          !products.length
           ? <h4 className="no-item">Add some products in the cart :)</h4>
           : props.products.map(item => (
             <article key={item.id} className="cart-item flex-between">
@@ -71,15 +75,13 @@ export default function Cart(props) {
         <div className="tatal-amount-div flex-between">
           <p>SUB TOTAL</p>
           <h4 className="total-price">
+            $ {totalPrice}
             {
-              props.products.length 
-              ? <>
-                  $ {totalPrice}
-                  <p className="cart-total-installment">
+              products.length 
+              ? <p className="cart-total-installment">
                     OR UP TO {installments} x ${(totalPrice / installments).toFixed(2)}
-                  </p>
-                </>
-              : "$ 0.00"
+                </p>
+              : ""
             }
           </h4>
         </div>
